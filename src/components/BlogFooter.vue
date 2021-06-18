e5db2568be6c7c5b3e71600571da7a63508d5b3a
Gabelox14 <61789851+Gabelox14@users.noreply.github.com>
2021-06-17 23:56:09 -0600

    0.1v

---
+++ src/components/BlogFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <transition appear name="v--mask">
+    <footer v-once class="blog__footer">
+      <a class="signature" href="https://snipcart.com" target="_blank">
+        <p>Hecho por SKEMClub</p>
+      </a>
+    </footer>
+  </transition>
+</template>
+
+<script>
+export default { name: 'blog-footer' }
+</script>
