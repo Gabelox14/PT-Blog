a272f4f47a72580108ef541845af241d091238ea
Gabelox14 <61789851+Gabelox14@users.noreply.github.com>
2021-08-26 09:02:55 -0600

    0.2v
    0.2v

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
-      <a class="signature" href="https://snipcart.com" target="_blank">
+      <a class="signature" href="https://github.com/Gabelox14/PT-Blog" target="_blank">
         <p>Hecho por SKEMClub</p>
       </a>
     </footer>
